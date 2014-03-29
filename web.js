cfbe2de072757e3167a184df02ccdd714dac816c
Nick Doiron <ndoiron@mapmeld.com>
2014-03-28 20:27:51 -0500

    remove express

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var http = require('http');
 var request = require('request').defaults({ encoding: 'binary' });
 var md5 = require('md5');
 var mm = require("memjs").Client
 
 var mj = mm.create();
 
-var app = express();
-
-app.get('/tiles/:layer/:z/:x/:y', function(req, res) {
+var server = http.createServer(function(req, res){
+  if(req.url.indexOf("tile") == -1){
+    return;
+  }
   var tile_val;
   try{
     tile_val = (md5.digest_s(req.url+"")+"");
@@ -16,9 +17,9 @@ app.get('/tiles/:layer/:z/:x/:y', function(req, res) {
     tile_val = md5(req.url+"");
   }
   mj.get(tile_val, function(err, tile_content) {
-    res.header('Content-Type', 'image/png');
+    res.setHeader('content-type', 'image/png');
     if (tile_content && tile_content != "undefined") {
-      res.send(new Buffer(tile_content, 'binary'));
+      return res.end(new Buffer(tile_content, 'binary'));
     }
     else{
       var requestOptions = {
@@ -28,13 +29,8 @@ app.get('/tiles/:layer/:z/:x/:y', function(req, res) {
       request(requestOptions, function (err, response, body) {
         var img_body = new Buffer(body, 'binary');
         mj.set(tile_val, img_body);
-        res.send(new Buffer(body, 'binary'));
+        return res.end(new Buffer(body, 'binary'));
       });
     }
   });
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+}).listen(process.env.PORT || 3000);
